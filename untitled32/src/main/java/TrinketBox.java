10f8aecabc560674e0b7544e1d165ba8ced4b836
Slava Maratkanov <slava_maratkanov@mail.ru>
2020-10-15 09:58:20 +0500

    delUntitled32

--- untitled32/src/main/java/TrinketBox.java
+++
@@ -1,9 +0,0 @@
-public class TrinketBox {
-
-   static volatile boolean toggle = true;
-   static TrinketBox trinket = new TrinketBox();
-
-   synchronized void set(boolean value){
-      toggle = value;
-   }
-}
